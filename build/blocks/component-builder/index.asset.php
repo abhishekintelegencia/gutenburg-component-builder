451074c5849975135a426a8a6379bf3678db0721
Abhishek Saini <abhishekintelegencia>
2026-03-25 16:45:39 +0530

    feature/button-adavnce-styling

--- build/blocks/component-builder/index.asset.php
+++ build/blocks/component-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7bd03ec12db7c51a13b8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3ff1265667eb9f982eb9');
